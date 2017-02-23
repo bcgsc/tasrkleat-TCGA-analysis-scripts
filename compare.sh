380d9dfeca2f698c47c3771434a395ffe3b85334
Zhuyi Xue <alfred532008@gmail.com>
2017-02-23 08:53:25 -0800

    combined compare_klt_ucsc.sh compare_klt_ens.sh into compare.sh

---
+++ compare.sh
@@ -0,0 +1,37 @@
+# these are the only four features available in klt.gz and ucsc.gz
+FEATURES="exon CDS start_codon stop_codon"
+OUTPUT=__diff
+mkdir -p ${OUTPUT}
+
+echo "# First, extracting features into separate files..."
+# NOTE: ens.gz uses 1 while the other two use chr1)
+for gtf_prefix in klt ucsc ens; do
+    for fea in ${FEATURES}; do
+        zcat ${gtf_prefix}.gz \
+            | \grep -P "^(?:chr)?1\t.*\t${fea}\t" \
+            | sed 's/^chr//g' \
+            | awk '{print $1,$3,$4,$5,$7}' \
+            | tr ' ' '\t' \
+            | sort -k 1n -k 3n -k 4n \
+            > ${OUTPUT}/${gtf_prefix}_${fea}.tsv &
+    done
+done
+wait
+
+echo 
+echo "# Comparing klt.gz & ucsc.gz..."
+echo "It should print out all 0s"
+for fea in ${FEATURES}; do
+    echo -n "comparing ${fea}: "
+    diff ${OUTPUT}/klt_${fea}.tsv ${OUTPUT}/ucsc_${fea}.tsv
+    echo $?
+done
+
+echo
+echo "# Comparing klt.gz & ens.gz..."
+echo "If the two are the same, the returncode should be 0"
+for fea in ${FEATURES}; do
+    echo -n "comparing ${fea}: "
+    sdiff ${OUTPUT}/klt_${fea}.tsv ${OUTPUT}/ens_${fea}.tsv > ${OUTPUT}/sdiff_${fea}.tsv
+    echo $?
+done
