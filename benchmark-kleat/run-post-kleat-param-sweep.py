1ef1c9dfd1cdf59f647899aae9f926807cba40d3
Zhuyi Xue <alfred532008@gmail.com>
2018-06-23 07:00:14 -0700

    added run-post-kleat-param-sweep.py

---
+++ benchmark-kleat/run-post-kleat-param-sweep.py
@@ -0,0 +1,19 @@
+HEXMAERS = ['AATAAA', 'ATTAAA', 'AGTAAA', 'TATAAA', 'CATAAA', 'GATAAA',
+            'AATATA', 'AATACA', 'AATAGA', 'AAAAAG', 'ACTAAA', 'AAGAAA',
+            'AATGAA', 'TTTAAA', 'AAAACA', 'GGGGCT']
+
+
+filter_style_A = 'A'
+for nda in range(15, 100, 10):
+    for ltc in range(0, 7, 2):
+        for nbr in range(0, 7, 2):
+            for maxbrtl in range(0, 7, 2):
+                for hxm in [HEXMAERS[:2], HEXMAERS]:
+                    hxm = ' '.join(hxm)
+                    # print('source activate ml && python post-kleat-param-sweep.py UHR/C1/tasrkleat-results/kleat/cba.KLEAT {nda} {ltc} {nbr} {maxbrtl}'.format(**locals()))
+                    print(f'python post-kleat-param-sweep.py UHR/C1/tasrkleat-results/kleat/cba.KLEAT {filter_style_A} {nda} {ltc} {nbr} {maxbrtl} {hxm}')
+
+filter_style_B = 'B'
+for tbr in range(1, 10):
+    print(f'python post-kleat-param-sweep.py UHR/C1/tasrkleat-results/kleat/cba.KLEAT {filter_style_B} {tbr}')
+
