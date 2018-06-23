2017953669df666fc8c9ecee72dd89b31bd9deb7
Zhuyi Xue <alfred532008@gmail.com>
2018-06-23 07:57:50 -0700

    added cat code to compare_to_polyA_Seq.sh

--- benchmark-kleat/compare_to_polyA_Seq.sh
+++ benchmark-kleat/compare_to_polyA_Seq.sh
@@ -7,3 +7,9 @@ done
 for i in $(\ls -v ./UHR/C1/tasrkleat-results/kleat/postproc-styleB-tbr-tuning/*.csv); do
     echo "python compare_to_polyA_Seq.py ${i} kleat"
 done
+
+
+# concatenate comparison results
+# cat ./UHR/C1/tasrkleat-results/kleat/postproc-styleA-polyA-confidence/*vs-polyA-Seq.csv  > UHR/C1/tasrkleat-results/kleat/styleA-filtered-benchmark.txt
+# cat ./UHR/C1/tasrkleat-results/kleat/postproc-styleB-tbr-tuning/*vs-polyA-Seq.csv  > UHR/C1/tasrkleat-results/kleat/styleB-filtered-benchmark.txt
+
